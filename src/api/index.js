5633fd811e5a8d45c7c743edf613db51fc85e7d5
xiaj <xiaj@lierda.com>
2018-12-03 17:33:40 +0800

    增加api接口文件

---
+++ src/api/index.js
@@ -0,0 +1,39 @@
+import wepy from 'wepy';
+import util from '../utils/util';
+import md5 from '../utils/md5';
+import tip from '../utils/tip';
+
+const APP_URL = 'http://test.mobile.com';
+// const APP_URL = 'https://reagent.caryue.com';
+const API_SECRET_KEY = 'mapp.kexueshengyin.com';
+const TIMESTAMP = util.getCurrentTime();
+const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase());
+
+const wxRequest = async(method = 'GET',params = {}, url) => {
+    let data = params || {};
+    if (wx.getStorageSync('token')) {
+        data.token = wx.getStorageSync('token');
+    } else {
+        data.token = '';
+    }
+    const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase());
+    let res = await wepy.request({
+        url: APP_URL + url,
+        method: method,
+        data: data,
+        header: { 'Content-Type': 'application/json' ,'Accept': 'application/json', 'time': TIMESTAMP,'sign':SIGN},
+    });
+
+    if(res.data.code === 200){
+        return res.data;
+    }else{
+        tip.toast(res.data.msg);
+        return false;
+    }
+};
+
+export default{
+    login: (method,params) => wxRequest(method,params, "/api/home/login"),
+    register: (method,params) => wxRequest(method,params, "/api/home/register"),
+    queryGoodsList: (method,params) => wxRequest(method,params, "/api/goods/queryGoodsList"),
+};
