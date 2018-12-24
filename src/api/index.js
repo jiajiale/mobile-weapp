66b093ad0a99e9fd2a079b6ac5d1f867498c8de2
xiaj <xiaj@lierda.com>
2018-12-24 16:02:22 +0800

    增加二维码生成

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 import wepy from 'wepy';
 import util from '../utils/util';
+import tip from '../utils/tip';
 import md5 from '../utils/md5';
 
-const APP_URL = 'http://mobile.caryue.com';
+// const APP_URL = 'http://mobile.caryue.com';
 const GOODS_IMG = 'http://www.caryue.com/Public/Uploads/goods/source/';
-// const APP_URL = 'https://reagent.caryue.com';
+const APP_URL = 'http://test.mobile.com';
 const API_SECRET_KEY = 'mapp.kexueshengyin.com';
 const TIMESTAMP = util.getCurrentTime();
 const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase());
@@ -23,10 +24,11 @@ const wxRequest = async(method = 'GET',params = {}, url) => {
         data: data,
         header: { 'Content-Type': 'application/json' ,'Accept': 'application/json', 'time': TIMESTAMP,'sign':SIGN},
     });
-    if(res.data.code === 200){
+    if(res.statusCode === 200){
         return res.data;
     }else{
-        return false;
+        tip.loaded();
+        tip.toast('网络请求失败');
     }
 };
 
@@ -35,4 +37,5 @@ export default{
     login: (method,params) => wxRequest(method,params, "/api/public/login"),
     register: (method,params) => wxRequest(method,params, "/api/public/register"),
     queryGoodsList: (method,params) => wxRequest(method,params, "/api/goods/index"),
+    addGoodsCart: (method,params) => wxRequest(method,params, "/api/cart/addGoodsCart"),
 };
---
+++ src/utils/msg.js
@@ -0,0 +1,7 @@
+export default{
+    LOADING: '正在加载中',
+    CARTING: '正在下单中',
+    MAKING_QR: '生成小程序码中',
+    SELECT_SPEC: '请选择规格',
+    SELECT_SERVICE: '请选择业务',
+};
