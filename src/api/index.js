183d0262ca66beee91fab32eabdea7a9499b379f
xiaj <xiaj@lierda.com>
2019-01-02 20:26:54 +0800

    更改小程序页面

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import tip from '../utils/tip';
 
 const APP_URL = 'https://mobile.caryue.com';
 // const APP_URL = 'http://test.mobile.com';
-const GOODS_IMG_URL = 'http://www.caryue.com/Public/Uploads/goods/source/';
+const GOODS_IMG_URL = 'https://mobile.caryue.com/Public/Uploads/goods/source/';
 const PHOTO_IMG_URL = APP_URL + '/Public/Uploads/photo/';
 const UPLOAD_IMG_URL = APP_URL + '/Admin/Image/uploadPicture';
 
@@ -34,6 +34,7 @@ export default{
     UPLOAD_IMG_URL: UPLOAD_IMG_URL,
     login: (method,params) => wxRequest(method,params, "/api/public/login"),
     register: (method,params) => wxRequest(method,params, "/api/public/register"),
+    updateUserPhone: (method,params) => wxRequest(method,params, "/api/public/updateUserPhone"),
     queryGoodsList: (method,params) => wxRequest(method,params, "/api/goods/index"),
     addGoodsCart: (method,params) => wxRequest(method,params, "/api/cart/addGoodsCart"),
     queryCartInfo: (method,params) => wxRequest(method,params, "/api/cart/getCartGoods"),
