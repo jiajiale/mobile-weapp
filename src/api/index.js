9f48d11a48de594a5d7fdc6c9662ba3e1e5d8535
xiaj <xiaj@lierda.com>
2019-01-02 18:59:37 +0800

    更新接口请求信息

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import wepy from 'wepy';
 import tip from '../utils/tip';
 
-const APP_URL = 'http://mobile.caryue.com';
+const APP_URL = 'https://mobile.caryue.com';
 // const APP_URL = 'http://test.mobile.com';
 const GOODS_IMG_URL = 'http://www.caryue.com/Public/Uploads/goods/source/';
 const PHOTO_IMG_URL = APP_URL + '/Public/Uploads/photo/';
