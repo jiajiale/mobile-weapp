02165e5c8bf2c2d0b886e376cf01d3c402c670d0
xiaj <xiaj@lierda.com>
2019-03-28 19:55:27 +0800

    增加购物车操作

--- src/utils/msg.js
+++ src/utils/msg.js
@@ -5,6 +5,7 @@ export default{
     CARTING: '正在下单中',
     MAKING_QR: '生成小程序码中',
     SELECT_SPEC: '请选择规格',
+    SELECT_GOODS: '请选择商品',
     SELECT_SERVICE: '请选择业务',
     ADD_CART_SUCCESS: '添加成功',
 };
