a7a0f2e621f5ef869d64b7ae495370150b5031d1
xiaj <xiaj@lierda.com>
2019-04-16 18:48:02 +0800

    更新小程序

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import wepy from 'wepy';
 import tip from '../utils/tip';
 
-const APP_URL = 'https://mobile.caryue.com';
+const APP_URL = 'https://www.caryue.com';
 // const APP_URL = 'http://111.230.96.241:81';
 const GOODS_IMG_URL = APP_URL + '/Public/Uploads/goods/source/';
 const CATEGORY_IMG_URL = APP_URL + '/Public/Uploads/category/source/';
